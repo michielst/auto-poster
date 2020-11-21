36791e8687ab2dd9fdb5fe7efdf37d6fb851c1a5
michielst <michieldegreef@outlook.com>
2020-11-21 13:10:46 +0100

    Change reddit fetch url

--- src/util/reddit.ts
+++ src/util/reddit.ts
@@ -5,9 +5,9 @@ import { RedditPostResponse } from '../models';
 export class Reddit {
     public REDDIT_URL = 'https://www.reddit.com';
 
-    getPosts(subreddit, limit = 5): Promise<RedditPostResponse[]> {
+    getPosts(subreddit, limit = 20): Promise<RedditPostResponse[]> {
         return new Promise(async (resolve, reject) => {
-            fetch(`${this.REDDIT_URL}/r/${subreddit}/hot/.json?limit=${limit}`, { method: 'GET' })
+            fetch(`${this.REDDIT_URL}/r/${subreddit}/top/.json?t=day&limit=${limit}`, { method: 'GET' })
                 .then(res => res.json())
                 .then(json => resolve(json.data.children.filter((post: RedditPostResponse) => !post.data.is_self)))
                 .catch(reject);
