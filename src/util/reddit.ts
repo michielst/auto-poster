459d6628f64a40012775d72541dd17a1f1c63674
michielst <michieldegreef@outlook.com>
2020-11-24 19:10:41 +0100

    Fetch posts from top week instead of day

--- src/util/reddit.ts
+++ src/util/reddit.ts
@@ -12,7 +12,7 @@ export class Reddit {
 
     getPosts(limit = 50): Promise<RedditPostResponse[]> {
         return new Promise(async (resolve, reject) => {
-            fetch(`${this.REDDIT_URL}/r/${this.subreddit}/top/.json?t=day&limit=${limit}`, { method: 'GET' })
+            fetch(`${this.REDDIT_URL}/r/${this.subreddit}/top/.json?t=week&limit=${limit}`, { method: 'GET' })
                 .then(res => res.json())
                 .then(json => resolve(json.data.children.filter((post: RedditPostResponse) => !post.data.is_self)))
                 .catch(reject);
