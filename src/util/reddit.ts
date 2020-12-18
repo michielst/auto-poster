49c494a867783554de7a0cdad880f3f0e29e1bf7
michielst <michieldegreef@outlook.com>
2020-12-18 03:52:31 +0100

    Filter out gif urls

--- src/util/reddit.ts
+++ src/util/reddit.ts
@@ -14,8 +14,20 @@ export class Reddit {
         return new Promise(async (resolve, reject) => {
             fetch(`${this.REDDIT_URL}/r/${this.subreddit}/top/.json?t=week&limit=${limit}`, { method: 'GET' })
                 .then(res => res.json())
-                .then(json => resolve(json.data.children.filter((post: RedditPostResponse) => !post.data.is_self && !post.data.is_video)))
+                .then(json => resolve(json.data.children.filter(
+                    (post: RedditPostResponse) => !post.data.is_self
+                        && !post.data.is_video
+                        && this.hasValidUrl(post.data.url)
+                )))
                 .catch(reject);
         })
+    }
+
+    private hasValidUrl(url: string): boolean {
+        if (/redgifs|gfycat|gif/.test(url)) {
+            return false;
+        }
+
+        return true;
     }
 }
