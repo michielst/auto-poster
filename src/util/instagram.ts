c734347994c9ff2ae3cca107e9f08f2a02d4113f
michielst <michieldegreef@outlook.com>
2021-01-01 22:42:38 +0100

    Use cookiestore

--- src/util/instagram.ts
+++ src/util/instagram.ts
@@ -1,15 +1,17 @@
 import Instagram from 'instagram-web-api';
 import { InstagramUploadType } from '../models';
 
+const FileCookieStore = require('tough-cookie-filestore2');
 
 export class InstagramWrapper {
     public client: Instagram;
 
     constructor(username: string, password: string) {
-        this.client = new Instagram({ username, password })
+        const cookieStore = new FileCookieStore('./cookies.json')
+        this.client = new Instagram({ username, password, cookieStore });
     }
 
     public async upload(photo: string, caption: string, type: InstagramUploadType) {
-        await this.client.uploadPhoto({ photo, caption, post: type });
+        return this.client.uploadPhoto({ photo, caption, post: type });
     }
 }
